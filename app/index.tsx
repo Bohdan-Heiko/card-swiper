d2e6c67ed89c5e43d832867f601325903330a5b5
gektor <bheiko@salerow.software>
2024-08-17 17:32:16 +0300

    card style

--- app/index.tsx
+++ app/index.tsx
@@ -1,13 +1,41 @@
-import { data } from "@/utils/data";
+import Card from "@/components/card";
+import { data, DataType } from "@/utils/data";
 import React from "react";
-import { StyleSheet } from "react-native";
+import { StyleSheet, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
+const MAX_CARD_ITEMS = 3;
 const Main = () => {
-  const [newData, setNewData] = React.useState([...data, ...data]);
-  return <SafeAreaView></SafeAreaView>;
+  const [newData, setNewData] = React.useState<DataType[]>([...data, ...data]);
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.cardContainer}>
+        {newData.map((item, index) => {
+          if (index > MAX_CARD_ITEMS) return null;
+          return (
+            <Card
+              key={index}
+              item={item}
+              index={index}
+              dataLength={newData.length}
+              maxVisibleItems={MAX_CARD_ITEMS}
+            />
+          );
+        })}
+      </View>
+    </SafeAreaView>
+  );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  cardContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
 
 export default Main;
---
+++ components/card.tsx
@@ -0,0 +1,103 @@
+import { DataType } from "@/utils/data";
+import React from "react";
+import { Image, StyleSheet, Text, View } from "react-native";
+
+type Props = {
+  // newData: DataType[];
+  // setNewData: React.Dispatch<React.SetStateAction<DataType[]>>;
+  item: DataType;
+  index: number;
+  dataLength: number;
+  maxVisibleItems: number;
+  // animatedValue: SharedValue<number>;
+  // currentIndex: number;
+  // setCurrentIndex: React.Dispatch<React.SetStateAction<number>>;
+};
+
+const Card = ({ item, index, dataLength, maxVisibleItems }: Props) => {
+  return (
+    <View
+      style={[
+        styles.container,
+        {
+          zIndex: dataLength - index,
+          backgroundColor: item.backgroundColor,
+          opacity: index < maxVisibleItems ? 1 : 0,
+          transform: [{ scale: 1 - index * 0.05 }, { translateY: index * -30 }],
+        },
+
+        // animatedStyle,
+      ]}
+    >
+      <View style={styles.top}>
+        <Text style={styles.textName}>{item.name}</Text>
+        <View style={styles.imageContainer}>
+          <Image source={item.image} style={styles.image} />
+        </View>
+      </View>
+      <View style={styles.middle}>
+        <Text style={styles.textNumber}>{item.number}</Text>
+      </View>
+      <View style={styles.bottom}>
+        <View>
+          <Text style={styles.text}>Valid thru</Text>
+          <Text style={styles.text}>{item.exp}</Text>
+        </View>
+        <View>
+          <Text style={styles.text}>Cvv</Text>
+          <Text style={styles.text}>{item.cvv}</Text>
+        </View>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    position: "absolute",
+    width: 360,
+    height: 200,
+    borderRadius: 28,
+    padding: 16,
+  },
+  top: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  textName: {
+    fontSize: 26,
+    fontWeight: "bold",
+    color: "white",
+  },
+  imageContainer: {
+    width: 80,
+    height: 40,
+  },
+  image: {
+    width: 80,
+    height: 40,
+    resizeMode: "contain",
+  },
+  middle: {
+    flex: 2,
+    justifyContent: "center",
+  },
+  textNumber: {
+    fontSize: 28,
+    fontWeight: "bold",
+    color: "white",
+  },
+  text: {
+    fontSize: 18,
+    color: "white",
+  },
+  bottom: {
+    flex: 1,
+    flexDirection: "row",
+    gap: 56,
+  },
+});
+
+export default Card;
